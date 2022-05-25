32eaf1481f71efb97a0e7b93d304c0f314e096ba
Zynthia <68821005+szynthia@users.noreply.github.com>
2022-05-25 15:49:33 -0700

    Added codes to get inventory lists.

--- src/main/java/com/animalshelter/capstone_project/controller/Controller.java
+++ src/main/java/com/animalshelter/capstone_project/controller/Controller.java
@@ -1,31 +1,24 @@
 package com.animalshelter.capstone_project.controller;
-
-import com.animalshelter.capstone_project.model.CatDog;
-import com.animalshelter.capstone_project.model.MedicalRecord;
-import com.animalshelter.capstone_project.model.Model;
+import com.animalshelter.capstone_project.model.*;
 import javafx.collections.ObservableList;
-
 import static com.animalshelter.capstone_project.model.Model.BINARY_FILE_ANIMALS;
 import static com.animalshelter.capstone_project.model.Model.BINARY_FILE_MEDICAL_RECORD;
 
 public class Controller {
-
     private static Controller theInstance;
     private ObservableList<CatDog> mAllAnimalsList;
     private ObservableList<MedicalRecord> mAllMedicalRecordsList;
-
+    private ObservableList<PerishableGoods> mPerishableGoods;
+    private ObservableList<NonPerishable> mNonPerishable;
     /**
      * Gets the one instance of the Controller.
      * @return The instance
      */
     public static Controller getInstance() {
         //if theInstance is null, create a new object
+        //otherwise, return theInstance
         if (theInstance ==null) {
             theInstance = new Controller();
-            //otherwise, return theInstance
-
-            //Fill the mAllAnimalsList with data from Model
-            //if the binary file has data, fill with binary file. Otherwise, fill with csv file
             if (Model.binaryFileHasData(BINARY_FILE_ANIMALS)) {
                 theInstance.mAllAnimalsList = Model.populateAnimalsListFromBinaryFile();
             } else {
@@ -36,32 +29,37 @@ public class Controller {
             } else {
                 theInstance.mAllMedicalRecordsList = Model.populateMedicalRecordsListFromCSVFile();
             }
-
+            if (Model.PGBinaryFileHasData()) {
+                theInstance.mPerishableGoods = Model.PGListFromBinaryFile();
+            } else {
+                theInstance.mPerishableGoods = Model.PGListFromTXTFile();
+            }
+            if (Model.NPGBinaryFileHasData()) {
+                theInstance.mNonPerishable = Model.NPGListFromBinaryFile();
+            } else {
+                theInstance.mNonPerishable = Model.NPGListFromTXTFile();
+            }
         }
         return theInstance;
     }
-
     private Controller() {
     }
-
-    /**
-     * Gets the list of all animals.
-     * @return The list of all animals.
-     */
     public ObservableList<CatDog> getAllAnimals() {
         return mAllAnimalsList;
     }
-
     public ObservableList<MedicalRecord> getAllMedicalRecords() {
         return mAllMedicalRecordsList;
     }
-
-    /**
-     * Makes a request for the model to save all the animals data (the list of all animals) to
-     * a persistent binary file.
-     */
+    public  ObservableList<PerishableGoods> getAllPG() {
+        return mPerishableGoods;
+    }
+    public  ObservableList<NonPerishable> getAllNPG() {
+        return mNonPerishable;
+    }
     public void saveData() {
         Model.writeDataToAnimalsBinaryFile(mAllAnimalsList);
         Model.writeDataToMedicalRecordsBinaryFile(mAllMedicalRecordsList);
+        Model.writeDataToPGBinaryFile(mPerishableGoods);
+        Model.writeDataToNPGBinaryFile(mNonPerishable);
     }
 }
