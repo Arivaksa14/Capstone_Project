fa60fe92491d4e7a383dc90102a09eaa4d1a176b
Zynthia <68821005+szynthia@users.noreply.github.com>
2022-05-25 16:06:46 -0700

    Update FoodSupplyScene.java

--- src/main/java/com/animalshelter/capstone_project/view/FoodSupplyScene.java
+++ src/main/java/com/animalshelter/capstone_project/view/FoodSupplyScene.java
@@ -1,11 +1,16 @@
 package com.animalshelter.capstone_project.view;
+import com.animalshelter.capstone_project.controller.Controller;
 import com.animalshelter.capstone_project.model.PerishableGoods;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import java.time.format.DateTimeFormatter;
 
 public class FoodSupplyScene extends Scene {
     public static final int WIDTH = 1000;
@@ -20,7 +25,7 @@ public class FoodSupplyScene extends Scene {
     private TextField productTF = new TextField();
     private Label productErrLabel = new Label("Product Name is required.");
 
-    private TextField vendor = new TextField();
+    private ComboBox<String> vendorCB = new ComboBox<>();
     private Label vendorErrLabel = new Label("Vendor is required.");
 
     private TextField priceTF = new TextField();
@@ -29,11 +34,17 @@ public class FoodSupplyScene extends Scene {
     private TextField specTF = new TextField();
     private Label specErrLabel = new Label("Spec is required.");
 
-    private TextField expireDateTF = new TextField();
+    private DatePicker expireDate = new DatePicker();
     private Label expireDateErrLabel = new Label("Expiration date is required.");
 
     private ListView<PerishableGoods> perishableGoodsLV = new ListView<>();
+    private ObservableList<PerishableGoods> PGList;
+    private Controller controller = Controller.getInstance();
+    private Button addButton = new Button("Add Product");
+    private Button removeButton = new Button("Remove Product");
     private Button returnButton = new Button("Return to Main Page");
+    private ImageView ohDog = new ImageView();
+    private PerishableGoods selectedPG;
 
     public FoodSupplyScene() {
         super(new GridPane(), WIDTH, HEIGHT);
@@ -44,62 +55,123 @@ public class FoodSupplyScene extends Scene {
         pane.setPadding(new Insets(5));
 
         // Column#1
-        pane.add(new Label("Item#:"), 0, 0);
-        pane.add(new Label("Category:"), 0, 1);
-        pane.add(new Label("Product Name:"), 0, 2);
-        pane.add(new Label("Vendor: "), 0, 3);
-        pane.add(new Label("Price: "), 0, 4);
-        pane.add(new Label("Spec: "), 0, 5);
-        pane.add(new Label("Expiration date: "), 0, 6);
+        pane.add(returnButton, 0, 0);
+        returnButton.setOnAction(e -> ViewNavigator.loadScene( "Animal Shelter Application", new MainScene()));
+
+        ohDog.setImage(new Image("HungryDog.png"));
+        ohDog.setFitWidth(460);
+        ohDog.setFitHeight(230);
+        pane.add(ohDog, 0, 1, 1, 8);
+
+        pane.add(perishableGoodsLV, 0, 9, 4, 1);
 
         // Column#2
-        pane.add(itemNoTF,1,0);
-        pane.add(categoryCB,1,1);
-        categoryCB.getItems().addAll("Cat Treat","Cat Food (Dry)","Cat Food (Wet)","Dog Treat","Dog Food (Dry)","Dog Food (Wet)");
-        categoryCB.getSelectionModel().select(-1);
-        pane.add(productTF,1,2);
-        pane.add(vendor,1,3);
-        pane.add(priceTF,1,4);
-        pane.add(specTF,1,5);
-        pane.add(expireDateTF,1,6);
+        pane.add(new Label("Item#:"), 1, 1);
+        pane.add(new Label("Category:"), 1, 2);
+        pane.add(new Label("Product Name:"), 1, 3);
+        pane.add(new Label("Vendor: "), 1, 4);
+        pane.add(new Label("Price: "), 1, 5);
+        pane.add(new Label("Spec: "), 1, 6);
+        pane.add(new Label("Expiration date: "), 1, 7);
 
         // Column#3
-        // Order: 0-Item#, 1-Category, 2-Product Name, 3-Vendor, 4-Price, 5-Spec, 6-Expiration Date
-        pane.add(itemNoErrLabel,2,0);
+        pane.add(itemNoTF,2,1);
+        pane.add(categoryCB,2,2);
+        categoryCB.getItems().addAll("Cat Treats","Cat Food (Dry)",
+                "Cat Food (Wet)","Dog Treats","Dog Food (Dry)",
+                "Dog Food (Wet)");
+        categoryCB.getSelectionModel().select(-1);
+        pane.add(productTF,2,3);
+        pane.add(vendorCB,2,4);
+        vendorCB.getItems().addAll("American Journey",
+                "Blue Buffalo", "Cat Chow", "Fancy Feast",
+                "Friskies", "Meow Mix", "Pedigree", "PureBites",
+                "Purina Beneful", "Shameless Pets", "SmartBones",
+                "Tiny Tiger", "True Acre Foods");
+
+        pane.add(priceTF,2,5);
+        pane.add(specTF,2,6);
+        pane.add(expireDate,2,7);
+        pane.add(addButton, 2, 8);
+        addButton.setOnAction(e -> addPG());
+
+        pane.add(removeButton,2,10);
+        removeButton.setOnAction(e -> removePG());
+        removeButton.setDisable(true);
+
+        // Column#4
+        pane.add(itemNoErrLabel,3,1);
         itemNoErrLabel.setTextFill(Color.RED);
         itemNoErrLabel.setVisible(false);
 
-        pane.add(categoryErrLabel,2,1);
+        pane.add(categoryErrLabel,3,2);
         categoryErrLabel.setTextFill(Color.RED);
         categoryErrLabel.setVisible(false);
 
-        pane.add(productErrLabel,2,2);
+        pane.add(productErrLabel,3,3);
         productErrLabel.setTextFill(Color.RED);
         productErrLabel.setVisible(false);
 
-        pane.add(vendorErrLabel,2,3);
+        pane.add(vendorErrLabel,3,4);
         vendorErrLabel.setTextFill(Color.RED);
         vendorErrLabel.setVisible(false);
 
-        pane.add(priceErrLabel,2,4);
+        pane.add(priceErrLabel,3,5);
         priceErrLabel.setTextFill(Color.RED);
         priceErrLabel.setVisible(false);
 
-        pane.add(specErrLabel,2,5);
+        pane.add(specErrLabel,3,6);
         specErrLabel.setTextFill(Color.RED);
         specErrLabel.setVisible(false);
 
-        pane.add(expireDateErrLabel,2,6);
+        pane.add(expireDateErrLabel,3,7);
         expireDateErrLabel.setTextFill(Color.RED);
         expireDateErrLabel.setVisible(false);
 
-
         perishableGoodsLV.setPrefWidth(WIDTH);
-        pane.add(perishableGoodsLV, 0, 8, 3, 1);
+        PGList = controller.getAllPG();
+        perishableGoodsLV.setItems(PGList);
+        perishableGoodsLV.getSelectionModel().selectedItemProperty().addListener((obsVal, oldVal, newVal) -> selectPG(newVal));
 
-        pane.add(returnButton, 0, 10);
-        returnButton.setOnAction(e -> ViewNavigator.loadScene( "Animal Shelter Application", new MainScene()));
         this.setRoot(pane);
     }
 
+    private void selectPG(PerishableGoods newVal) {
+        selectedPG = newVal;
+        removeButton.setDisable(selectedPG==null);
+    }
+    private void removePG() {
+        PGList.remove(selectedPG);
+        perishableGoodsLV.refresh();
+        perishableGoodsLV.getSelectionModel().select(-1);
+    }
+
+    private void updateDisplay(){
+        perishableGoodsLV.refresh();
+    }
+    private void addPG() {
+        // Order: 0-Item#, 1-Category, 2-Product Name, 3-Vendor, 4-Price, 5-Spec, 6-Expiration Date
+        itemNoErrLabel.setVisible(itemNoTF.getText().isEmpty());
+        categoryErrLabel.setVisible(categoryCB.getSelectionModel().getSelectedIndex()==-1);
+        productErrLabel.setVisible(productTF.getText().isEmpty());
+        vendorErrLabel.setVisible(vendorCB.getSelectionModel().getSelectedIndex()==-1);
+        priceErrLabel.setVisible(priceTF.getText().isEmpty());
+        specErrLabel.setVisible(specTF.getText().isEmpty());
+        expireDateErrLabel.setVisible(expireDate.getValue() == null);
+        if (itemNoErrLabel.isVisible() || categoryErrLabel.isVisible() ||
+                productErrLabel.isVisible() || vendorErrLabel.isVisible() ||
+                priceErrLabel.isVisible() || specErrLabel.isVisible() ||
+                expireDateErrLabel.isVisible())
+            return;
+        else{
+            PGList.add(0,
+                    new PerishableGoods(Integer.parseInt(itemNoTF.getText()),
+                            categoryCB.getSelectionModel().getSelectedItem(),
+                            productTF.getText(),
+                            vendorCB.getSelectionModel().getSelectedItem(),
+                            Double.parseDouble(priceTF.getText()),
+                            specTF.getText(),
+                            expireDate.getValue().format(DateTimeFormatter.ofPattern("M/d/yyyy"))));
+        }
+    }
 }
