ba060810e356126a82a665b04a96a43eaf7658f0
Zynthia <68821005+szynthia@users.noreply.github.com>
2022-05-25 16:04:39 -0700

    Create NonPerishable.java

---
+++ src/main/java/com/animalshelter/capstone_project/model/NonPerishable.java
@@ -0,0 +1,26 @@
+package com.animalshelter.capstone_project.model;
+import java.io.Serializable;
+import java.text.NumberFormat;
+
+public class NonPerishable extends Inventory implements Comparable<Inventory>, Serializable {
+    String mCategory, mSize;
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
+    public NonPerishable(int itemNo, String category, String productName, String vendor, double price, String size) {
+        super(itemNo, productName, vendor, price);
+        mCategory = category;
+        mSize = size;
+    }
+    public int compareTo(NonPerishable o) {
+        return super.compareTo(o);
+    }
+
+    @Override
+    public String toString() {
+        return "Item#: " + mItemNo +
+                ", " + mCategory +
+                ", " + mProductName +
+                ", " + mVendor +
+                ", " + currency.format(mPrice) +
+                ", " + mSize;
+    }
+}
