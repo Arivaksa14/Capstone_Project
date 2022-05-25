bff5f807e145e731f517b7293dddfca2ac9a3033
Zynthia <68821005+szynthia@users.noreply.github.com>
2022-05-25 16:03:48 -0700

    Updated with toString

--- src/main/java/com/animalshelter/capstone_project/model/PerishableGoods.java
+++ src/main/java/com/animalshelter/capstone_project/model/PerishableGoods.java
@@ -1,16 +1,27 @@
 package com.animalshelter.capstone_project.model;
 import java.io.Serializable;
+import java.text.NumberFormat;
+
 public class PerishableGoods extends Inventory implements Comparable<Inventory>, Serializable {
     String mCategory, mSpec,mExpDate;
-
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
     public PerishableGoods(int itemNo, String category, String productName, String vendor, double price, String spec, String expireDate) {
         super(itemNo, productName, vendor, price);
         mCategory = category;
         mSpec = spec;
         mExpDate = expireDate;
     }
-
     public int compareTo(PerishableGoods o) {
     return super.compareTo(o);
+    }
+    @Override
+    public String toString() {
+        return "Item#: " + mItemNo +
+                ", " + mCategory +
+                ", " + mProductName +
+                ", " + mVendor +
+                ", " + currency.format(mPrice) +
+                ", " + mSpec +
+                ", Expiration Date: " + mExpDate;
     }
 }
